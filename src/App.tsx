ba5de6e28b92f1a59f11ed7892e784151faf93cd
Dustine Trieu <dtrieu7679@gmail.com>
2024-02-16 18:54:49 -0500

    Function 1 Task 4

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App(): JSX.Element {
                     <Col>
                         <div
                             style={{
-                                width: 30,
+                                width: "50%",
                                 height: 100,
                                 backgroundColor: "#FF0000"
                             }}
@@ -43,7 +43,7 @@ function App(): JSX.Element {
                     <Col>
                         <div
                             style={{
-                                width: 30,
+                                width: "50%",
                                 height: 100,
                                 backgroundColor: "#FF0000"
                             }}
