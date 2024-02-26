94d648a8cbc071ca4e0ad997175cc36e5ed17f28
Dustine Trieu <dtrieu7679@gmail.com>
2024-02-26 18:43:32 -0500

    Funtion 1 & 2 finished Task 7

--- src/nested.ts
+++ src/nested.ts
@@ -6,7 +6,10 @@ import { Question, QuestionType } from "./interfaces/question";
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-    return [];
+    const PublishedQuestion = questions.filter(
+        (question: Question): boolean => question.published
+    );
+    return PublishedQuestion;
 }
 
 /**
@@ -15,7 +18,13 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    const NonEmpty = questions.filter(
+        (question: Question): boolean =>
+            question.body !== "" ||
+            question.expected !== "" ||
+            question.options.length > 0
+    );
+    return NonEmpty;
 }
 
 /***
