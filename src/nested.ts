63c728cf0c24ab819832c7cdadcf8797b8fcb2b4
Dustine Trieu <dtrieu7679@gmail.com>
2024-02-26 18:57:32 -0500

    Function 6 Finished Task 7

--- src/nested.ts
+++ src/nested.ts
@@ -67,7 +67,12 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const Total = questions.reduce(
+        (Currentsum: number, question: Question) =>
+            Currentsum + question.points,
+        0
+    );
+    return Total;
 }
 
 /***
