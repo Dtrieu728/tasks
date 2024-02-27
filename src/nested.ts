e8995f534f0601357fddd6fd34919a065ff778ab
Dustine Trieu <dtrieu7679@gmail.com>
2024-02-26 19:59:25 -0500

    Function 8 Task 7 finished :,(

--- src/nested.ts
+++ src/nested.ts
@@ -108,7 +108,13 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const QuestionCSV = questions
+        .map(
+            (question: Question): string =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`
+        )
+        .join("\n");
+    return "id,name,options,points,published\n" + QuestionCSV;
 }
 
 /**
