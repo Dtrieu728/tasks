b5277a0853caaf4969359bb8865363a4d0d3a653
Dustine Trieu <dtrieu7679@gmail.com>
2024-03-05 20:02:08 -0500

    Finxed doubleHalf and CHoose Team

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App(): JSX.Element {
                 <span style={{ color: "blue" }}>Dustine Trieu</span>
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
