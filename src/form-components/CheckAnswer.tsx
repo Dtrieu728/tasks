069270c2104963ecff979e025b30eb6b3cd04f29
Dustine Trieu <dtrieu7679@gmail.com>
2024-03-07 12:56:51 -0500

    CheckAnswer component task 10

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [UserAnswer, setUserAnswer] = useState<string>("");
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserAnswer(event.target.value);
+    }
+    const isCorrect = UserAnswer === expectedAnswer;
     return (
         <div>
-            <h3>Check Answer</h3>
+            <h3>
+                <Form.Group controlId="formMovieName">
+                    <Form.Label>Input Answer here:</Form.Label>
+                    <Form.Control value={UserAnswer} onChange={updateAnswer} />
+                </Form.Group>
+            </h3>
+            {isCorrect ? (
+                <span style={{ color: "green" }}>✔️</span>
+            ) : (
+                <span style={{ color: "red" }}>❌</span>
+            )}
         </div>
     );
 }
